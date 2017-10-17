94c50d06bfacc2c71d0b72bd8e0bf0478f1cbb4a
Louis Atkin <32098761+Atkin-L-BPC-Magenta@users.noreply.github.com>
2017-10-17 14:14:07 +0100

    Numeric values added

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
 
 <?php
 
-$mytext1="Hello everyone!";
-$mytext2="How are you?";
+$mytext1 = "Hello everyone!";
+$mytext2 = "How are you?";
 
+$posWorld = strpos($mytext1, "Campers");
+$IngMyTxt1 = strlen($mytext1);
+$IngMyTxt2 = strlen($mytext2);  
+  
 echo "The first string is: '";
 echo $mytext1;
 echo "'<br/><br/>";
@@ -14,6 +18,13 @@ echo "The second string is: '";
 echo $mytext2;
 echo "'<br/><br/>";
 
+echo "The number of characters in the first string is: '";
+echo $IngMyTxt1;
+echo "'<br/><br/>";
+echo "The number of characters in the second string is: '";
+echo $IngMyTxt2;
+echo "'<br/><br/>";
+  
 ?>
 
 <BODY>
