79387bd50a9308db5574f8674b1c0afca17a764e
Louis Atkin <32098761+Atkin-L-BPC-Magenta@users.noreply.github.com>
2017-10-17 13:57:31 +0100

    uploaded a new variable text

---
+++ index.php
@@ -0,0 +1,21 @@
+<HTML>
+
+<BODY>
+
+<?php
+
+$mytext1="Hello everyone!";
+$mytext2="How are you?";
+
+echo "The first string is: '";
+echo $mytext1;
+echo "'<br/><br/>";
+echo "The second string is: '";
+echo $mytext2;
+echo "'<br/><br/>";
+
+?>
+
+<BODY>
+
+<HTML>
